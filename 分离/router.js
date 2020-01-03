4cdee9b97ffee1d69746df495e5cfcd0ff319cc8
caohefei <caohefei@gokuaidian.com>
2020-01-03 17:52:29 +0800

    分离

--- 分离/router.js
+++ 分离/router.js
@@ -1,1 +1,18 @@
 //  router  的分离
+
+
+const router = require('koa-router')()
+const controller=require('./controller')
+
+module.exports=(app)=>{
+  router.get('/',controller.index)
+
+  router.get('/home',controller.home)
+
+  router.get('/home/:id/:name',controller.homeParams)
+  router.get('/user',controller.login )
+   router.post('/user/register',controller.register)
+
+  app.use(router.routes()).use(router.allowedMethods())
+
+}
