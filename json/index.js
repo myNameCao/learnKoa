4c18c80fe46c7cbc2a9294e787ff4334c19f0f2e
caohefei <caohefei@gokuaidian.com>
2020-01-06 20:29:20 +0800

    revise log

--- json/index.js
+++ json/index.js
@@ -7,6 +7,6 @@ const middleware = require('./middleware')
 middleware(app)
 router(app)
 app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000')
+  console.log('json test start_ listen 3000')
 })
 
--- log/index.js
+++ log/index.js
@@ -5,6 +5,6 @@ const middleware = require('./middleware')
 
 middleware(app)
 app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000')
+  console.log('log test start listening 3000')
 })
 
