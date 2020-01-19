bfdf820d10a54264cf2086153eb096d3ec1b6df9
曹合飞 <caohefei@gokuaidian.com>
2020-01-19 10:31:23 +0800

    add post

---
+++ test/post.js
@@ -0,0 +1,68 @@
+
+
+const Koa = require('koa')
+const app = new Koa()
+
+
+
+
+
+app.use(async ctx =>{
+  if(ctx.url==='/'&&ctx.method=='GET'){
+    let html = `
+    <form method="POST" action="/">
+      <p>userName</p>
+      <input name="userName" /><br/>
+      <p>nickName</p>
+      <input name="nickName" /><br/>
+      <p>email</p>
+      <input name="email" /><br/>
+      <button type="submit">submit</button>
+    </form>
+   `
+   ctx.body = html
+  }else if (ctx.url==='/'&&ctx.method==='POST'){
+   let postData = await parsePostData(ctx)
+   ctx.body = postData
+  }else{
+    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+  }
+})
+
+function parsePostData (ctx){
+  return new Promise((resolve,reject)=>{
+     try{
+       let postdata=''
+       ctx.req.addListener('data',(data)=>{
+      
+        postdata += data
+       })
+       ctx.req.addListener('end',()=>{
+           let parseData = parseQueryStr(postdata)
+           resolve(parseData)
+       })
+     }catch(err){
+         reject(err)
+     }
+  })
+}
+
+
+function parseQueryStr( queryStr ) {
+  let queryData = {}
+  console.log(queryStr)
+  let queryStrList = queryStr.split('&')
+  console.log( queryStrList.entries())
+  for (  let [ index, queryStr ] of queryStrList.entries()  ) {
+    let itemList = queryStr.split('=')
+    queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
+  }
+  return queryData
+}
+
+
+
+
+app.listen(3000,function(){
+  console.log('start lister 3000')
+})
