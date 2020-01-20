6ea8e3a64907375a534e36fedb575cb5c91e8e74
曹合飞 <caohefei@gokuaidian.com>
2020-01-20 14:02:39 +0800

    add sql

--- middleware/sql.js
+++ middleware/sql.js
@@ -1,10 +1,17 @@
 const mysql      = require('mysql')
+
+const fs = require('fs');
+
+
 const pool = mysql.createPool({
   host     : '127.0.0.1',   // 数据库地址
   user     : 'root',    // 数据库用户
   password : 'chris',   // 数据库密码
   database : 'chris'  // 选中数据库
 })
+
+
+let sqlContentMap = {}
 
 let  query = function(sql,values){
   return new Promise((resolve,reject)=>{
@@ -26,14 +33,86 @@ let  query = function(sql,values){
   })
 }
 
+const walkFile = function(  pathResolve , mime ){
 
-module.exports= async function(ext,next){
+  let files = fs.readdirSync( pathResolve )
 
-  let sql = 'SELECT * FROM _mysql_session_store'
-  let dataList = await query( sql )
-  console.log(dataList)
-  await next()
-   
+  let fileList = {}
+
+   for( let [ i, item] of files.entries() ) {
+    let itemArr = item.split('\.')
+
+    let itemMime = ( itemArr.length > 1 ) ? itemArr[ itemArr.length - 1 ] : 'undefined'
+    let keyName = item + ''
+    if( mime === itemMime ) {
+      fileList[ item ] =  pathResolve + item
+    }
+  }
+
+  return fileList
 }
+
+
+function getSqlContent( fileName,  path ) {
+  let content = fs.readFileSync( path, 'binary' )
+  sqlContentMap[ fileName ] = content
+}
+function getSqlMap () {
+  let basePath = __dirname
+  basePath = basePath.replace(/\\/g, '\/')
+
+  let pathArr = basePath.split('\/')
+  pathArr = pathArr.splice( 0, pathArr.length - 1 )
+  basePath = pathArr.join('/') + '/sql/'
+
+  let fileList = walkFile( basePath, 'sql' )
+  return fileList
+}
+
+
+function getSqlContentMap () {
+  let sqlMap = getSqlMap()
+  for( let key in sqlMap ) {
+    getSqlContent( key, sqlMap[key] )
+  }
+
+  return sqlContentMap
+}
+
+
+const eventLog = function( err , sqlFile, index ) {
+  if( err ) {
+    console.log(`[ERROR] sql脚本文件: ${sqlFile} 第${index + 1}条脚本 执行失败 o(╯□╰)o ！`)
+  } else {
+    console.log(`[SUCCESS] sql脚本文件: ${sqlFile} 第${index + 1}条脚本 执行成功 O(∩_∩)O !`)
+  }
+}
+
+
+let sqlContentMaps = getSqlContentMap()
+
+// 执行建表sql脚本
+const createAllTables = async (ctx,next) => {
+  for( let key in sqlContentMaps ) {
+    let sqlShell = sqlContentMaps[key]
+    let sqlShellList = sqlShell.split(';')
+
+    for ( let [ i, shell ] of sqlShellList.entries() ) {
+      if ( shell.trim() ) {
+        let result = await query( shell )
+        if ( result.serverStatus * 1 === 2 ) {
+          eventLog( null,  key, i)
+        } else {
+          eventLog( true,  key, i) 
+        }
+      }
+    }
+  }
+  console.log('sql脚本执行结束！')
+  console.log('请按 ctrl + c 键退出！')
+  
+   await next()
+}
+module.exports= createAllTables
 
 
