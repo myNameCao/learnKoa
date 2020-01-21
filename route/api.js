73df0c892d1b36aac4dd4aa833817ed30c9095db
曹合飞 <caohefei@gokuaidian.com>
2020-01-21 16:42:51 +0800

    add

--- route/api.js
+++ route/api.js
@@ -2,12 +2,12 @@ const router = require('koa-router')({
   prefix: '/api'
 })
 
-router.all('/*', async (ctx, next) => {
+router.all('/*', async (ctx,next) => {
   // *代表允许来自所有域名请求
   ctx.set("Access-Control-Allow-Origin", "*");
   await next();
 });
-router.get('/',async function(ctx,next){
+router.get('/',async function(ctx){
   ctx.response.body={
     code:9527,
     result:{
@@ -15,7 +15,7 @@ router.get('/',async function(ctx,next){
     }
   }
 })
-router.get('/shareInfo',async function(ctx,next){
+router.get('/shareInfo',async function(ctx){
   ctx.response.body={
     code:9527,
     result:{
